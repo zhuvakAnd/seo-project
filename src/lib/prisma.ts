24d409d8032cacddade2cfa3340e7eec305de1cd
Zhuvak Bubba Andrii <zhuvak.andrii@gmail.com>
2026-02-28 14:12:17 +0200

    Fix prisma error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,19 @@
-import { prisma } from '../../lib/prisma';
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = prisma;
+declare global {
+  // allow global.prisma in dev so we don't instantiate more than one client
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    // any options you need
+  });
+
+// only assign in development – in production a new instance is fine
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
